44c1aa3a7d5f4298f719216eb64c8ec57c1ec5bf
Marios Andreopoulos <opensource@andmarios.com>
2015-08-19 12:04:24 +0300

    some basic grid usage

--- mTermTemp.go
+++ mTermTemp.go
@@ -40,7 +40,7 @@ func main() {
 	}
 
 	counter := time.Tick(1 * time.Second)
-	counterH_Tick := 30
+	counterH_Tick := 2
 	counterH := make(chan int, 1) // counterH comes from counter. If it was independent, we wouldn't be able to make sure that counterH ticks always between counterH_Tick runs of counter
 
 	err := termui.Init()
@@ -51,10 +51,10 @@ func main() {
 	bc := termui.NewBarChart()
 	bc.Border.Label = " CPU Temperatures (°C)"
 	bc.TextColor = termui.ColorMagenta
-	bc.Width = 58
+	//	bc.Width = 58
 	bc.Height = 16
 	bc.DataLabels = label
-	bc.BarWidth = 10
+	//	bc.BarWidth = 10
 	bc.BarGap = 1
 	bc.SetMax(95)
 	bc.BarColor = termui.ColorRed
@@ -64,43 +64,43 @@ func main() {
 	// LineChart
 	lc0 := termui.NewLineChart()
 	lc0.Border.Label = " " + label[0] + add_to_label
-	lc0.Width = 58
+	//	lc0.Width = 58
 	lc0.Height = 16
-	lc0.X = 58 + 1
+	//	lc0.X = 58 + 1
 	lc0.LineColor = termui.ColorCyan | termui.AttrBold
 	//	lc0.Mode = "dot"
 
 	lc1 := termui.NewLineChart()
 	lc1.Border.Label = " " + label[1] + add_to_label
-	lc1.Width = 58
+	//	lc1.Width = 58
 	lc1.Height = 16
-	lc1.Y = 16 + 1
+	//	lc1.Y = 16 + 1
 	lc1.LineColor = termui.ColorCyan | termui.AttrBold
 	//	lc1.Mode = "dot"
 
 	lc2 := termui.NewLineChart()
 	lc2.Border.Label = " " + label[2] + add_to_label
-	lc2.Width = 58
+	//	lc2.Width = 58
 	lc2.Height = 16
-	lc2.X = 58 + 1
-	lc2.Y = 16 + 1
+	//	lc2.X = 58 + 1
+	//	lc2.Y = 16 + 1
 	lc2.LineColor = termui.ColorCyan | termui.AttrBold
 	//	lc2.Mode = "dot"
 
 	lc3 := termui.NewLineChart()
 	lc3.Border.Label = " " + label[3] + add_to_label
-	lc3.Width = 58
+	//	lc3.Width = 58
 	lc3.Height = 16
-	lc3.Y = 16 + 1 + 16 + 1
-	lc3.LineColor = termui.ColorCyan | termui.AttrBold
+	//	lc3.Y = 16 + 1 + 16 + 1
+	//	lc3.LineColor = termui.ColorCyan | termui.AttrBold
 	//	lc3.Mode = "dot"
 
 	lc4 := termui.NewLineChart()
 	lc4.Border.Label = " " + label[4] + add_to_label
-	lc4.Width = 58
+	//	lc4.Width = 58
 	lc4.Height = 16
-	lc4.X = 58 + 1
-	lc4.Y = 16 + 1 + 16 + 1
+	//	lc4.X = 58 + 1
+	//	lc4.Y = 16 + 1 + 16 + 1
 	lc4.LineColor = termui.ColorCyan | termui.AttrBold
 	//	lc4.Mode = "dot"
 
@@ -116,6 +116,21 @@ func main() {
 	lc2.Data = temperature_history[2]
 	lc3.Data = temperature_history[3]
 	lc4.Data = temperature_history[4]
+
+	termui.Body.AddRows(
+		termui.NewRow(
+			termui.NewCol(6, 0, bc),
+			termui.NewCol(6, 0, lc0)),
+		termui.NewRow(
+			termui.NewCol(6, 0, lc1),
+			termui.NewCol(6, 0, lc2)),
+		termui.NewRow(
+			termui.NewCol(6, 0, lc3),
+			termui.NewCol(6, 0, lc4)))
+	termui.Body.Align()
+	bc.BarWidth = ((termui.Body.Width / 2) - 2 - len(temp_files)) / len(temp_files)
+
+	termui.Render(termui.Body)
 
 	// MainLoop:
 	rotate := 0
@@ -140,7 +155,8 @@ func main() {
 				counterH <- 1
 				rotate = 0
 			}
-			termui.Render(bc, lc0, lc1, lc2, lc3, lc4)
+			// termui.Render(bc, lc0, lc1, lc2, lc3, lc4)
+			termui.Render(termui.Body)
 		case <-counterH:
 			for i := range temperature_history {
 				temperature_history[i] = append(temperature_history[i][1:], temperature_temp_sum[i]/float64(counterH_Tick))
@@ -155,6 +171,12 @@ func main() {
 			// break MainLoop
 			if e.Type == termui.EventKey {
 				return
+			}
+			if e.Type == termui.EventResize {
+				termui.Body.Width = termui.TermWidth()
+				termui.Body.Align()
+				bc.BarWidth = ((termui.Body.Width / 2) - 2 - len(temp_files)) / len(temp_files)
+				termui.Render(termui.Body)
 			}
 		}
 	}
